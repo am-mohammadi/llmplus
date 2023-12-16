6679f871ace5a7162d4dc6058524548d883dd2c9
amirmahdi mohammadi <mohamadi.mahdi.6@gmail.com>
2023-12-16 12:33:48 +0330

    v.0.0.4

--- src/llmplus/ChatGot/api.py
+++ src/llmplus/ChatGot/api.py
@@ -1,11 +1,30 @@
 import requests
 from json import loads
-import authorization
 import Models
 
 
+def headers(authorization):
+    h = {
+        'authority': 'chatgot-ai.chatgot.io',
+        'accept': '*/*',
+        'accept-language': 'en-US,en;q=0.9',
+        'authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJtbWRpLmpvYkBnbWFpbC5jb20iLCJleHBpcmUiOjE3MDM5ODA4MDAwMDAsInJhbmRvbSI6IjJmYjgxMTFkYWU3YWRkYmVhOGFlZmU2MjNkZjNkZDIyIn0=.lmeREpHYMi9YWUkaPI8PNIjYWzj7CYHAR4qbO3vlI9k=',
+        'cache-control': 'no-cache',
+        'content-type': 'application/json',
+        'origin': 'https://start.chatgot.io',
+        'pragma': 'no-cache',
+        'referer': 'https://start.chatgot.io/',
+        'sec-ch-ua': '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-site',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+    }
+    return h
 
-class ChatGot:
+class API:
     def __init__(self, authorization_key):
         self.authorization_key = authorization_key
     
@@ -59,9 +78,9 @@ class ChatGot:
     
 if __name__ == '__main__':
     authorization_key = 'authorization_key'
-    cg = ChatGot(authorization_key)
+    api = API(authorization_key)
     
-    cg.model = Models.Claude2()
-    answer = cg.send('who is the manager of the real madrid?')
+    api.model = Models.Claude2()
+    answer = api.send('who is the manager of the real madrid?')
     
     # asnwer2  = cg.send('who the CEO?')
